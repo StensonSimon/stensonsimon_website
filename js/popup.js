7811fb8ab45eb61b80f2c9d62b1b708a8d21b828
stensonsimon <stensonsimon@gmail.com>
2021-08-05 20:10:00 +0100

    fixed position of the popup

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,20 @@
 function togglePopup(){
     document.getElementById("review-popup").classList.toggle("active");
+    document.body.style.overflow = "hidden"; // ADD THIS LINE
+    document.body.style.height = "100%"; // ADD THIS LINE
+
+}
+// When the user clicks on <span> (x), close the modal
+function closePopup() {
+    document.getElementById("review-popup").classList.toggle("active");
+    document.body.style.overflow = "auto"; // ADD THIS LINE
+    document.body.style.height = "auto"; // ADD THIS LINE
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = "auto"; // ADD THIS LINE
+        document.body.style.height = "auto";  // ADD THIS LINE
+    }
 }
